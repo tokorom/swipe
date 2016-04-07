8ceb9042eb92cbd761f2252850d2e1f6696c5a46
tokorom <tokorom@gmail.com>
2016-04-07 15:35:10 +0900

    Add swipe-iOS

---
+++ Sources/swipe.h
@@ -0,0 +1,19 @@
+//
+//  swipe.h
+//  swipe
+//
+//  Created by tokorom on 4/7/16.
+//  Copyright © 2016 Swipe, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swipe.
+FOUNDATION_EXPORT double swipeVersionNumber;
+
+//! Project version string for swipe.
+FOUNDATION_EXPORT const unsigned char swipeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swipe/PublicHeader.h>
+
+
